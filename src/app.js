3b219a5a41a76bf3292d7fc1b8f9e8bd5b13e2fe
Carlos Silva <carlhmath@gmail.com>
2026-01-21 18:34:57 +0000

    se vuelve a trabajar en los archivos correspondientes y se borran los que no eran necesarios

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+function generateExcuse() {
+  let who = ['El perro', 'Mi abuela', 'El cartero', 'Mi pájaro'];
+  let action = ['comió', 'orino', 'rompió', 'perdió'];
+  let what = ['mi tarea', 'mi celular', 'las llaves'];
+  let when = ['antes de la clase', 'cuando dormía', 'mientras hacía ejercicio', 'durante mi almuerzo'];
+  let maxexcuse = ["y me duele la panza..", "y no tengo internet..", "y me fui de viaje.."];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+  let maxexcuseIndex = Math.floor(Math.random() * maxexcuse.length);
+
+  let excuse = `${who[whoIndex]} ${action[actionIndex]} ${what[whatIndex]} ${when[whenIndex]} ${maxexcuse[maxexcuseIndex]}   `;
+  return excuse;
+}
+
+
+window.onload = function () {
+  document.getElementById('excuse').innerHTML = generateExcuse();
 };
+
+
+
